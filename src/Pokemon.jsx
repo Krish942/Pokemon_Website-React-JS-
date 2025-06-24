73bd0ddd58399b17117dc3abbfd6fd684366da50
Krish jain <142033638+Krish942@users.noreply.github.com>
2025-06-24 14:08:19 +0530

    Update Pokemon.jsx

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -65,7 +65,7 @@ import { PokemonCards } from "./PokemonCards";
     <>
       <section className="container">
         <header>
-          <h1> Hello Pokkemon hehhee</h1>
+          <h1> Hello Pokkemon hehhee1234</h1>
         </header>
         <div className="pokemon-search">
           <input
