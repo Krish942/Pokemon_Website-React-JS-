4d81271319fa989c3407185cab0a8286521eaacd
Krish jain <142033638+Krish942@users.noreply.github.com>
2025-06-26 12:25:58 +0530

    Update Pokemon.jsx

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -93,7 +93,7 @@ const Pokemon = () => {
   return (
     <section className="container">
       <header>
-        <h1>Let's Catch Pokémon</h1>
+        <h1>Let's Catch Pokémon ok</h1>
       </header>
 
       <div className="controls">
